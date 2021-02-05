f26e54e1ba184fbd2c5cd8e308616de1aab7b5d7
Tom McKelvey <tommck@us.ibm.com>
2021-02-05 11:36:49 -0500

    sum file fixed and mult test broken

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
 };
--- tests/mult.test.js
+++ tests/mult.test.js
@@ -1,6 +1,6 @@
-const mult = require('../src/mult')
+const mult = require('../src/mult');
 
-it('should return correct product', () => {
-    const result = mult(2, 5)
-    expect(result).toBe(10)
-})
+test('should return correct product', () => {
+  const result = mult(2, 5);
+  expect(result).toBe(10);
+});
